f219c3d22a381cf83172b215383c166ab5199374
DESKTOP-31KA96E\Admin <kaiyuanpupu@outlook.com>
2024-04-21 15:03:41 +0800

    feat: 添加全局错误处理页面

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  error: object
+}>()
+
+const handleBackHome = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <a-result
+    :status="error.statusCode"
+    :title="error.statusCode"
+    :sub-title="error.message"
+  >
+    <template #extra>
+      <a-button type="primary" @click="handleBackHome">回到首页</a-button>
+    </template>
+  </a-result>
+</template>
