428db2aecc16c2e3fc804c774a882b440c8f74fd
jinxiao <jinxiao@asiainfo.com>
2024-04-25 17:15:18 +0800

    tijiao

--- components/wangEditor.vue
+++ components/wangEditor.vue
@@ -1,57 +1,5 @@
 <template>
   <div>
-    <!-- <Sidebar /> -->
-    <!-- <a-form layout="vertical">
-      <a-form-item>
-        <a-input
-          placeholder="输入标题（必填）"
-          type="text"
-          v-model:value="data.title" />
-      </a-form-item>
-      <a-form-item>
-        <a-textarea
-          placeholder="输入概述，用于展示概述"
-          :rows="5"
-          v-model:value="data.description" />
-      </a-form-item>
-    </a-form> -->
-
-    <!-- <div style="border: 1px solid #ccc">
-        <Toolbar
-          style="border-bottom: 1px solid #ccc"
-          :editor="editorRef"
-          :defaultConfig="toolbarConfig"
-          :mode="mode" />
-        <Editor
-          style="height: 500px; overflow-y: hidden"
-          v-model="data.content"
-          :defaultConfig="editorConfig"
-          :mode="mode"
-          @onCreated="handleCreated" />
-      </div> -->
-    <!-- class="flex flex-col items-center w-full" -->
-
-    <!-- <div class="flex flex-col items-center w-full  min-h-screen">
-      <Toolbar
-        class="w-full"
-        :editor="editorRef"
-        :defaultConfig="toolbarConfig"
-        :mode="mode"
-        style="border-bottom: 1px solid #ccc" />
-      <Editor
-        class="w-full max-w-3xl min-h-96 bg-white shadow-md"
-        :defaultConfig="editorConfig"
-        :mode="mode"
-        v-model="valueHtml"
-        @onCreated="handleCreated"
-        @onChange="handleChange"
-        @onDestroyed="handleDestroyed"
-        @onFocus="handleFocus"
-        @onBlur="handleBlur"
-        @customAlert="customAlert"
-        @customPaste="customPaste" />
-    </div> -->
-    <!-- <a-affix :offset-top="0"> -->
     <a-affix :offset-top="navClientHeight">
       <div style="border-bottom: 1px solid #e8e8e8">
         <div id="editor-toolbar" class="bg-gray-light bg-opacity-5"></div>
@@ -82,20 +30,9 @@
   </div>
 </template>
 <script setup>
-// import "@wangeditor/editor/dist/css/style.css"; // 引入 css
-
 import { onBeforeUnmount, ref, shallowRef, onMounted, nextTick } from "vue";
-// import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
-// import "quill/dist/quill.bubble.css";
-// import "quill/dist/quill.snow.css";
-// import hljs from "highlight.js";
-// import "highlight.js/styles/monokai-sublime.css";
-// import Quill from "quill";
-// import "@wangeditor/editor/dist/css/style.css";
-// import Editor from "@wangeditor/editor-for-vue";
 import "@/static/editor/editor.js";
 import { message } from "ant-design-vue";
-// const mode = ref("default");
 const E = ref(window.wangEditor);
 const data = ref({
   title: "",
@@ -103,13 +40,6 @@ const data = ref({
   content: "",
 });
 const navClientHeight = ref(0);
-// // 模拟 ajax 异步获取内容
-// onMounted(() => {});
-// // 编辑器实例，必须用 shallowRef，重要！
-// const editorRef = shallowRef();
-
-// // 内容 HTML
-// const valueHtml = ref("<p>hello</p>");
 nextTick(() => {
   navClientHeight.value = document.getElementById("nav").clientHeight || 48;
   const editorConfig = {
@@ -118,7 +48,6 @@ nextTick(() => {
     MENU_CONF: {
       insertImage: {
         async parseImageSrc(src) {
-          // console.log("转换图片链接", src);
           // 创建一个Promise
           const loadImagePromise = new Promise((resolve, reject) => {
             // 创建一个新的Image对象
@@ -131,8 +60,6 @@ nextTick(() => {
               var width = this.width;
               var height = this.height;
               // 输出图片的宽度和高度
-              // console.log("Width:", width);
-              // console.log("Height:", height); // 创建一个Canvas元素
               var canvas = document.createElement("canvas");
               var ctx = canvas.getContext("2d");
               // 将图片绘制到Canvas上
@@ -161,19 +88,12 @@ nextTick(() => {
               // 计算压缩前后图片的内存大小
               var originalFileSize = srcBlob.size;
               var compressedFileSize = compressedImageBlob.size;
-              // console.log("原始图片大小:", originalFileSize / 1024, "kb");
-              // console.log("压缩后图片大小:", compressedFileSize / 1024, "kb");
-              // console.log("-----------------------------------------");
-              // 将Canvas中的图像转换为WebP格式
               canvas.toBlob(function (blob) {
                 // 创建一个URL对象，表示生成的WebP图像
                 var webpURL = URL.createObjectURL(blob);
-
                 // 解决Promise并返回生成的WebP图像的URL
                 resolve({ webpURL, compressedImageData });
               }, "image/webp");
-              // 解决Promise并返回修改后的file对象
-              // resolve(compressedImageData);
             };
           });
           const { webpURL, compressedImageData } = await loadImagePromise;
@@ -200,49 +120,20 @@ nextTick(() => {
                 res(compressedImageData);
               });
           });
-
-          // const uploadedJson = uploaded.json();
-          // console.log(uploadedJson);
-          // if (uploadedJson.file) {
-          //   // 等待Promise执行完成并返回结果
-          //   return (
-          //     useRuntimeConfig().public.static + uploadedJson.file.filename
-          //   );
-          // }
-        },
-        async onInsertedImage(file) {
-          // console.log("插入之前", file);
         },
       },
       uploadImage: {
         fieldName: "like-yuque-fileName",
         base64LimitSize: 10 * 1024 * 1024, // 10M 以下插入 base64
-        onBeforeUpload(file) {
-          console.log("更新图片之前", file);
-        },
-        customInsert(file) {
-          console.log("插入图片customInsert", file);
-        },
       },
     },
     uploadImgShowBase64: true,
   };
-  // editorConfig.placeholder = "请输入内容";
-  // editorConfig.scroll = false; // 禁止编辑器滚动
-  // editorConfig.MENU_CONF["uploadImage"] = {
-  //   fieldName: "like-yuque-fileName",
-  //   base64LimitSize: 10 * 1024 * 1024, // 10M 以下插入 base64
-  // };
-  // editorConfig.onChange = (editor) => {
-  //   console.log('content', editor.children)
-  //   // data.value.content = 
-  // };
 
   // 先创建 editor
   const editor = E.value.createEditor({
     selector: "#editor-text-area",
     content: [],
-    // html: '',
     config: editorConfig,
   });
 
@@ -262,48 +153,8 @@ nextTick(() => {
       editor.focus(true); // focus 到末尾
     }
   });
-  setTimeout(() => {
-    // valueHtml.value = "<p>模拟 Ajax 异步设置内容</p>";
-  }, 1500);
   editor.on("change", (e) => {
-    // console.log(editor.getHtml());
     data.value.content = editor.getHtml() || "";
-    // 定义正则表达式来匹配base64图片的完整字符串
-    // const regex = /data:image\/\w+;base64,([^"]+)/g;
-    // // 用正则表达式匹配所有base64图片的完整字符串，并打印出来
-    // let match;
-    // const promises = [];
-    // while ((match = regex.exec(data.value.content)) !== null) {
-    //   // 使用闭包来捕获每次迭代的match[0]值
-    //   (function (match) {
-    //     // 压缩base64图片并保存promise
-    //     promises.push(
-    //       compressBase64Image(match[0], 0.5) // quality设置为0.5，表示压缩质量为50%
-    //         .then((compressedBase64) => {
-    //           // 将压缩后的base64字符串替换原有位置
-    //           data.value.content = data.value.content.replace(
-    //             match[0],
-    //             compressedBase64
-    //           );
-    //           // 打印压缩前后的结果
-    //           console.log("原始base64图片：", match[0]);
-    //           console.log("压缩后的base64图片：", compressedBase64);
-    //           console.log(
-    //             "--------------------------------------",
-    //             data.value.content
-    //           );
-    //         })
-    //     );
-    //   })(match);
-    // }
-    // // 等待所有压缩操作完成
-    // Promise.all(promises)
-    //   .then(() => {
-    //     console.log("所有图片压缩完成");
-    //   })
-    //   .catch((error) => {
-    //     console.error("压缩base64图片时出错：", error);
-    //   });
   });
 });
 // 压缩base64图片的函数
@@ -330,76 +181,6 @@ function compressBase64Image(base64String, quality) {
 }
 // 模拟 ajax 异步获取内容
 onMounted(() => {});
-
-// const toolbarConfig = {
-//   excludeKeys: "fullScreen",
-// };
-// const editorConfig = {
-//   placeholder: "请输入内容...",
-//   MENU_CONF: {
-//     uploadImage: {
-//       base64LimitSize: 10 * 1024 * 1024, //10兆
-//     },
-//   },
-// };
-
-// // 组件销毁时，也及时销毁编辑器，重要！
-// onBeforeUnmount(() => {
-//   const editor = editorRef.value;
-//   if (editor == null) return;
-
-//   editor.destroy();
-// });
-
-// // 编辑器回调函数
-// const handleCreated = (editor) => {
-//   console.log("created", editor);
-//   editorRef.value = editor; // 记录 editor 实例，重要！
-// };
-// const handleChange = (editor) => {
-//   console.log("change:", editor.getHtml());
-// };
-// const handleDestroyed = (editor) => {
-//   console.log("destroyed", editor);
-// };
-// const handleFocus = (editor) => {
-//   console.log("focus", editor);
-// };
-// const handleBlur = (editor) => {
-//   console.log("blur", editor);
-// };
-// const customAlert = (info, type) => {
-//   alert(`【自定义提示】${type} - ${info}`);
-// };
-// const customPaste = (editor, event, callback) => {
-//   console.log("ClipboardEvent 粘贴事件对象", event);
-
-//   // 自定义插入内容
-//   // editor.insertText("xxx");
-
-//   // 返回值（注意，vue 事件的返回值，不能用 return）
-//   // callback(false); // 返回 false ，阻止默认粘贴行为
-//   callback(true); // 返回 true ，继续默认的粘贴行为
-// };
-
-// const insertText = () => {
-//   const editor = editorRef.value;
-//   if (editor == null) return;
-
-//   editor.insertText("hello world");
-// };
-
-// const printHtml = () => {
-//   const editor = editorRef.value;
-//   if (editor == null) return;
-//   console.log(editor.getHtml());
-// };
-
-// const disable = () => {
-//   const editor = editorRef.value;
-//   if (editor == null) return;
-//   editor.disable();
-// };
 
 const save = () => {
   const params = {
@@ -443,14 +224,12 @@ const save = () => {
 
 #content {
   height: calc(100% - 40px);
-  /* background-color: rgb(245, 245, 245); */
   overflow-y: auto;
   position: relative;
 }
 
 #editor-container {
   width: 100%;
-  /* margin: 30px auto 150px auto; */
   background-color: #fff;
   padding: 20px 50px 50px 50px;
   border: 1px solid #e8e8e8;
